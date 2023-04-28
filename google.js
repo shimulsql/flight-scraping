78778ed1d62ad5653bc18d15d3b93919a65b371d
Mostafizur Rahman <44481263+shimulsql@users.noreply.github.com>
2023-04-28 20:07:53 +0600

    add google scrap for testing

---
+++ google.js
@@ -0,0 +1,25 @@
+import puppeteer from "puppeteer";
+
+async function run(){
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto("https://www.google.com/");
+
+    await page.waitForSelector("input[type='submit']");
+
+    let searchBtnText = await page.$eval("input[type='submit']", (e) => {
+      return e.value;
+    })
+
+    console.log(searchBtnText);
+
+    // await browser.close();
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+run();
